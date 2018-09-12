da0c78dde38f8a0b903f92cfbac43e08147a4f41
Kaiyao <ustkyqiao@gmail.com>
2018-09-12 11:44:39 +0800

    fix typo

--- RE/test.py
+++ RE/test.py
@@ -4,6 +4,6 @@ import matplotlib.pyplot as plt
 Melt = np.loadtxt('./test.txt', skiprows=1)
 labels = ['Temp', 'E_pair', 'E_mol', 'TotEng', 'Press']
 for i, label in enumerate(labels, start=1):
-    plt.plot(Melt[1:, 0], Melt[1:, 4], label=label)
+    plt.plot(Melt[1:, 0], Melt[1:, i], label=label)
     plt.legend()
 plt.show()
